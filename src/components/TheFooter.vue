23c3b2527b130c0ebd6464f8de55da72c52e1210
胡奇良 <hql@ipms.cn>
2023-03-20 17:19:59 +0800

    feat: 多语言配置

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -5,8 +5,6 @@ const toggleLocales = () => {
   // change to some real logic
   // const locales = availableLocales
   // locale.value = locales[(locales.indexOf(locale.value) + 1) % locales.length]
-  console.log(locale.value);
-
   locale.value = locale.value === "zh-CN" ? "en-US" : "zh-CN"
 }
 </script>
@@ -16,10 +14,6 @@ const toggleLocales = () => {
     <RouterLink class="icon-btn mx-2" to="/" :title="t('button.home')">
       <div i-carbon-campsite />
     </RouterLink>
-
-    <button class="icon-btn mx-2 !outline-none" :title="t('button.toggle_dark')" @click="toggleDark()">
-      <div i="carbon-sun dark:carbon-moon" />
-    </button>
 
     <a class="icon-btn mx-2" :title="t('button.toggle_langs')" @click="toggleLocales()">
       <div i-carbon-language />
--- src/modules/i18n.ts
+++ src/modules/i18n.ts
@@ -1,33 +1,26 @@
 import { createI18n } from 'vue-i18n'
 import { type UserModule } from '~/types'
 import _ from "lodash-es"
+import { localLanguage } from "../../locales/npmLangs"
 
-import iviewEn from 'view-ui-plus/dist/locale/en-US';
-import proEn from 'pro_iview_v3/lib/locales/en-US';
-
-// Import i18n resources
-// https://vitejs.dev/guide/features.html#glob-import
-//
-// Don't need this? Try vitesse-lite: https://github.com/antfu/vitesse-lite
 const messages = Object.fromEntries(
   Object.entries(
-    import.meta.glob<{ default: any }>('../../locales/*.js', { eager: true }))
+    import.meta.glob<{ default: any }>('../../locales/*.json', { eager: true }))
     .map(([key, value]) => {
-      console.log(key, value);
-      return [key.slice(14, -3), value.default]
-      // const yaml = key.endsWith('.yaml')
-      // return [key.slice(14, yaml ? -5 : -4), value.default]
+      let realValue = value.default
+      const lang = key.slice(14, -5);
+      if (_.has(localLanguage, lang)) {
+        realValue = _.assign({}, _.get(localLanguage, lang), value.default)
+      }
+      return [lang, realValue]
     }),
 )
 
 export const install: UserModule = ({ app }) => {
-  console.log(messages);
-
   const i18n = createI18n({
     legacy: false,
     locale: 'zh-CN',
     messages
   })
-
   app.use(i18n)
 }
