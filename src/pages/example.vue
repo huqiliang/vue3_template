98546e38392281c4c6d1be14822efa0cde8b8254
胡奇良 <hql@ipms.cn>
2023-03-29 17:30:54 +0800

    feat: 修改多语言

--- src/pages/example.vue
+++ src/pages/example.vue
@@ -3,122 +3,128 @@ export default {
   name: 'Example',
   data() {
     return {
+      search: {
+        searchLineNum: 3, //默认一行几个 可以配合columns中的searchLineNum
+        value: { descript: '初始数据' }, //初始值
+        upDown: false //默认收起
+      },
+      cascader_data: [
+        {
+          value: 'beijing',
+          label: '北京',
+          children: [
+            {
+              value: 'gugong',
+              label: '故宫'
+            },
+            {
+              value: 'tiantan',
+              label: '天坛'
+            },
+            {
+              value: 'wangfujing',
+              label: '王府井'
+            }
+          ]
+        }
+      ],
       datas: [],
-      count: 0,
-
+      count: 0
     }
   },
   computed: {
     config() {
       return {
-        search: { searchLineNum: 3, value: { descript: 'gg' } },
         form: {
           labelPosition: 'right',
           formLineNum: 3,
           modalWidth: 70,
-          value: { createUser10: 'gg' },
+          value: { createUser10: 'gg' }
         },
         table: {
-          onOnRowDblclick: this.tableRowClick,
+          onOnRowDblclick: this.tableRowClick
         },
-        pageSize: 30,
-        count: 0,
-        hide: { table: false },
         toolBarActions: [
           'new',
           {
             key: 'test',
-            renderItem() {
+            renderItem: () => {
               return (
-              <Button
-                type="primary"
-                onclick={() => {
-                  this.$Message.info('自定义按钮操作')
-                }}
-              >
-                自定义
-              </Button>
+                <Button
+                  type="primary"
+                  onclick={() => {
+                    this.$Message.info('自定义按钮操作')
+                  }}
+                >
+                  {this.$t('custom')}
+                </Button>
               )
-            },
+            }
           },
           'fullscreen',
           'refresh',
-          'rowSetting',
+          'rowSetting'
         ],
-
-        cascader_data: [
-          {
-            value: 'beijing',
-            label: '北京',
-            children: [
-              {
-                value: 'gugong',
-                label: '故宫',
-              },
-              {
-                value: 'tiantan',
-                label: '天坛',
-              },
-              {
-                value: 'wangfujing',
-                label: '王府井',
-              },
-            ],
-          },
-        ],
-        toolBar: true,
-        autoData: {
-          columns: [
-            {
-              title: '标题',
-              key: 'toolBar',
-              type: 'radio',
-              props: {
-                value: true,
-              },
-            },
-          ],
-          value: {
-            toolBar: false,
-          },
-        },
         format: {
-        // formatCurrent({ page }) {
-        //   const { current, pageSize } = page;
-        //   return { ...page, current: (current - 1) * pageSize };
-        // }
-          formatCurrent: '(page.current - 1) * page.pageSize',
+          // formatCurrent({ page }) {
+          //   const { current, pageSize } = page;
+          //   return { ...page, current: (current - 1) * pageSize };
+          // }
+          formatCurrent: '(page.current - 1) * page.pageSize'
         },
         columns: [
+          {
+            title: '简介',
+            group: '分组A',
+            tooltip: true,
+            tooltipMaxWidth: 700,
+            key: 'descript',
+            renderTable: {
+              type: 'span',
+              useExp: true,
+              text: "value===200?'成功':'失败';console.log(value);",
+              style: {
+                color: "value==200?'#19be6b':'#ed4014'"
+              }
+            },
+            renderForm: {
+              type: 'i-input',
+              props: {
+                type: 'textarea',
+                rows: 4
+              }
+            },
+            formLineNum: 2
+          },
           {
             title: this.$t('example.is'),
             group: '分组A',
             key: 'cataLog',
             disabled: true,
             renderSearch: {
-              type: 'Radio',
+              type: 'Radio'
             },
             renderForm: {
               type: 'DatePicker',
               props: {
                 placeholder: 'lastDate',
-                format: 'yyyy-MM-dd hh:mm:ss',
+                format: 'yyyy-MM-dd hh:mm:ss'
               },
               format(value) {
                 return value ? dayjs(value).format('YYYY-MM-DD hh:mm:ss') : ''
-              },
+              }
             },
             renderTable: () => {
               return (
-              <Button
-                onclick={() => {
-                  console.log('aaa')
-                }}
-              >
-                aaa
-              </Button>
+                <Button
+                  onclick={() => {
+                    console.log('aaa')
+                  }}
+                >
+                  aaa
+                </Button>
               )
-            },
+            }
           },
           {
             title: this.$t('example.createUser'),
@@ -126,51 +132,51 @@ export default {
             key: 'createUser',
             renderSearch: ({ value, input }) => {
               return (
-              <Select value={value} oninput={input} clearable>
-                <Option value="1">1</Option>
-              </Select>
+                <Select value={value} oninput={input} clearable>
+                  <Option value="1">1</Option>
+                </Select>
               )
-            },
+            }
           },
           {
             title: '长度',
             group: '分组A',
             disabled: true,
             renderSearch: {
-              type: 'Slider',
+              type: 'Slider'
             },
             renderForm: {
               type: 'DatePicker',
               style: {
-                width: '100%',
-              },
+                width: '100%'
+              }
             },
             renderTable: (h, params) => {
               return <span>{params.row.isAllow === 'F' ? '黑名单' : '白名单'}</span>
             },
-            key: 'createUser2',
+            key: 'createUser2'
           },
           {
             title: '数字',
             renderSearch: {
-              type: 'InputNumber',
+              type: 'InputNumber'
             },
             renderForm: {
-              type: 'i-switch',
+              type: 'i-switch'
             },
-            key: 'createUser8',
+            key: 'createUser8'
           },
           {
             title: '城市',
             notShowTable: true,
             renderForm: {
               type: 'Select',
-              children: [],
+              children: []
             },
-            renderSearch: ({ value, input }) => {
-              return <Cascader value={value || []} onInput={input} data={this.cascader_data} clearable></Cascader>
+            renderSearch: () => {
+              return <Cascader data={this.cascader_data} clearable></Cascader>
             },
-            key: 'createUser4',
+            key: 'city'
           },
           {
             title: 'creat4',
@@ -179,8 +185,8 @@ export default {
             rules: [
               {
                 required: true,
-                message: 'creat4不能为空',
-              },
+                message: 'creat4不能为空'
+              }
             ],
             renderForm: {
               type: 'Cascader',
@@ -193,225 +199,149 @@ export default {
                     children: [
                       {
                         value: 'gugong',
-                        label: '故宫',
+                        label: '故宫'
                       },
                       {
                         value: 'tiantan',
-                        label: '天坛',
+                        label: '天坛'
                       },
                       {
                         value: 'wangfujing',
-                        label: '王府井',
-                      },
-                    ],
-                  },
-                ],
-              },
+                        label: '王府井'
+                      }
+                    ]
+                  }
+                ]
+              }
             },
-            key: 'createUs3er4',
+            key: 'createUs3er4'
           },
-          {
-            title: '简介',
-            tooltip: true,
-            tooltipMaxWidth: 700,
-            key: 'descript',
-            renderTable: {
-              type: 'span',
-              useExp: true,
-              text: 'value===200?\'成功\':\'失败\';console.log(value);',
-              style: {
-                color: 'value==200?\'#19be6b\':\'#ed4014\'',
-              },
-            },
-            renderForm: {
-              type: 'i-input',
-              props: {
-                type: 'textarea',
-                rows: 4,
-              },
-            },
-            formLineNum: 2,
-          },
+
           {
             title: '百分比',
             key: 'percent',
             notShowSearch: true,
             renderTable({ row }) {
               return (
-              <div class="p-5">
-                <Circle percent={row.percent} size={80}>
-                  <span class="demo-Circle-inner" style="font-size:14px">
-                    {row.percent}%
-                  </span>
-                </Circle>
-              </div>
+                <div class="p-5">
+                  <Circle percent={row.percent} size={80}>
+                    <span class="demo-Circle-inner" style="font-size:14px">
+                      {row.percent}%
+                    </span>
+                  </Circle>
+                </div>
               )
-            },
+            }
           },
           {
             title: '操作',
             key: 'action',
             actions: [
               {
+                title: '新增示例',
+                type: 'new'
+              },
+              {
                 type: 'edit',
-                title: '编辑2',
-                request: '/api/users/${cataLog}',
-                method: 'PUT',
+                title: '编辑示例',
+                request: '/api/users/${id}',
+                method: 'POST'
+              },
+
+              {
+                title: '阻止默认',
+                prevent: true,
+                action: params => {
+                  this.$Message.info({
+                    content: `用户是${params.row.createUser}`
+                  })
+                }
               },
               {
-                title: '新增',
-                type: 'new',
-              },
-              {
-                title: '测试',
-                action(params) {
-                  console.log('====================================')
-                  console.log(params)
-                  console.log('====================================')
-                },
-              },
-              {
-                title: '编辑',
+                title: '方法请求',
                 method: 'POST',
                 type: 'edit',
-                request: 'http://192.168.0.38:3000/mock/106/api/test',
-              // request(options) {
-              //   console.log(options);
-              //   return axios.post(
-              //     "http://192.168.0.38:3000/mock/67/sys/sysOptionList",
-              //     {
-              //       a: options.row.cateLog
-              //     }
-              //   );
-              // }
+                request(options) {
+                  return axios.post('http://192.168.0.38:3000/mock/67/sys/sysOptionList', {
+                    a: options.row.cateLog
+                  })
+                }
               },
               {
                 type: 'delete',
                 title: '普通删除',
                 request: 'http://192.168.0.38:3000/mock/106/api/table/',
-                method: 'DELETE',
+                method: 'DELETE'
               },
 
               {
                 type: 'delete',
-                title: '带参删除',
+                title: '链接带参删除',
                 request: 'http://192.168.0.38:3000/mock/106/api/table/${id}',
-                method: 'DELETE',
+                method: 'DELETE'
               },
-            // {
-            //   title: "删除",
-            //   type: "delete",
-            //   // method: "delete",
-            //   // request: "/id",
-            //   keys: ["id"]
-            // }
-            ],
-          },
+              {
+                title: '请求体带参删除',
+                type: 'delete',
+                request: 'http://192.168.0.38:3000/mock/106/api/table',
+                method: 'DELETE',
+                keys: ['id']
+              }
+            ]
+          }
         ],
         request: 'http://192.168.0.38:3000/mock/106/api/table',
-        // request(options) {
-        //   // console.log(options);
-        //   return axios.get("http://192.168.0.38:3000/mock/106/api/table", {
-        //     headers: {
-        //       nomsg: true
-        //     },
-        //     params: { ...options }
-        //   });
-        // },
         map: {
           dataPath: 'data',
           totalPath: 'totalRows',
-          message: 'message',
+          message: 'message'
         },
-        submitForm: 'http://192.168.0.38:3000/mock/106/api/test',
+        submitForm: 'http://192.168.0.38:3000/mock/106/api/test'
       }
-    },
-  },
-  async mounted() {
-    // const res = await this.$http.get(
-    //   "http://192.168.0.38:3000/mock/106/api/table",
-    //   {
-    //     headers: {
-    //       nomsg: true
-    //     }
-    //   }
-    // );
-    // console.log(this.columns[5]);
-    // console.log("====================================");
-    // this.columns[5].renderForm.children = [
-    //   {
-    //     type: "i-option",
-    //     props: {
-    //       value: "aa",
-    //       label: "New York"
-    //     }
-    //   }
-    // ];
-    // this.$success(res);
+    }
   },
   methods: {
     tableRowClick(row, index) {
       this.$Message.info(`你双击了第 ${index} 行 百分比是 ${row.percent}`)
     },
     changeTableData() {
-      this.datas[0].createUser = `修改数据 ${this.count++}`
+      this.datas[0].createUser = `修改表格数据 ${this.count++}`
     },
     changeSearchFresh() {
-      this.config.search.value.descript = '123'
+      this.search.value.descript = `修改搜索数据 ${this.count++}`
       this.$refs.table.fetch()
-      // this.toolBar = false;
-      // this.$set(this.data, 0, {
-      //   createUser: 'aa',
-      // })
+    },
+    searchHandle() {
+      this.$refs.table.fetch()
+    },
+    searchReset() {
+      this.$refs.table.searchReset()
     },
     table() {
       const ref = this.$refs.proSearch
       console.log(ref.value)
-    },
-    searchReset() {
-      this.$Message.info('重置了')
-    },
-  },
+    }
+  }
 }
 </script>
 
 <template>
   <div>
     <div flex items-center>
-      <div mr2>
-        搜索相关:
-      </div>
+      <div mr2>搜索相关:</div>
       <div>
-        <Button @click="changeSearchFresh">
-          修改简介并刷新表格
-        </Button>
+        <Button @click="changeSearchFresh" class="mr2"> 修改简介并刷新表格 </Button>
+        <Button @click="searchHandle" class="mr2"> 手动搜索 </Button>
+        <Button @click="searchReset"> 外部搜索重置 </Button>
       </div>
     </div>
     <div flex items-center mt-2>
-      <div mr2>
-        表格相关:
-      </div>
+      <div mr2>表格相关:</div>
       <div>
-        <Button @click="changeTableData">
-          修改表格数据
-        </Button>
+        <Button @click="changeTableData"> 修改表格数据 </Button>
       </div>
     </div>
-    <div flex items-center mt-2>
-      <div mr2>
-        表单相关:
-      </div>
-      <div>
-        <Button @click="changeTableData">
-          修改表格数据
-        </Button>
-      </div>
-    </div>
-    <pro-table
-      ref="table"
-      v-model="datas"
-      v-bind="config"
-    />
+    <pro-table ref="table" v-model="datas" v-bind="config" :search="search" />
 
     <!-- <pro-table :columns="columns" :searchLineNum="5"> </pro-table> -->
   </div>
